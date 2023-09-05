2690966c50ec106245da2299fbb364ffd775a4dd
reshmakapuluru <kapulurureshma024@gmail.com>
2023-09-05 11:04:46 +0530

    ki

--- src/Shoppingcart.jsx
+++ src/Shoppingcart.jsx
@@ -6,51 +6,62 @@ function Shoppingcart(){
     React.useEffect(function(){
       axios.get('https://fakestoreapi.com/products')
       .then((res)=>{
-        var products = res.data.map((y)=>{
-          return {...y,IsProductInCart:false,count:0,total:0}
+        var items = res.data.map((y)=>{
+          return {...y,IsProductInCart:false,count:0}
       });
-        setproducts([...products])
-      })
-    },[])
+        setproducts(items)
+      });
+
+    },[]);
    
     
-    function addtocart(a,i,t){
+    function addtocart(i){
        var temp=[...products]
        temp[i].IsProductInCart=true;
        temp[i].count++;
        setcart([...cart,temp[i]])
-    }
+       console.log(products);
+       
+    } 
     function inc(i){
       var temp=[...cart]
        temp[i].count++
         setcart(temp)
+        console.log(products);
     }
     function dec(i){
       var temp=[...cart]
        temp[i].count--
         setcart(temp)
+        console.log(products);
     }
     function remove(ind){
         var temp=[...cart]
         temp.splice(ind,1)
         setcart(temp,ind)
+        var x=products.findIndex((c)=>{
+            return (c.id===b.id)
+        })
+        products[x].IsProductInCart=false;
+        products[x].count=0;
+        console.log(products)
     }
     return(
         <div className="d-flex flex-wrap">
             <div style={{width:'55%'}}>
-                <h1>Products</h1>
+            <center><h1>PRODUCTS</h1></center>
             <ul className="d-flex flex-wrap">
             {
                 products.map((a,i)=>{
-                    return <div  className="card shadow-lg p-3 mb-5 bg-white rounded m-4 bg-info-subtle text-emphasis-info" style={{width: '14rem'}}>
+                    return <div className="card shadow-lg p-3 rounded m-4 " style={{width: '14rem',backgroundColor:'lightpink'}}>
                     
-                    <div className="card-body d-flex flex-column justify-content-between"  >
+                    <div className="card-body d-flex flex-column justify-content-between">
                         <img src={a.image} style={{height:'160px',width:'160px'}} alt="Card image cap"/>
                         <h5 className="card-title">{a.title}</h5>
                         <p className="card-text">{a.price}</p>
                     <div>
                     
-                    <button  className="btn btn-info" onClick={()=>{addtocart(a,i)}}  disabled={a.IsProductInCart?true:false}>Add to cart</button>
+                    <button  className="btn btn-info" onClick={()=>{addtocart(i)}}  disabled={a.IsProductInCart?true:false}>Add to cart</button>
                       
                       
                     </div>
@@ -61,41 +72,39 @@ function Shoppingcart(){
             }
            </ul>
             </div>
-            <div className='p-3'style={{width:'45%',border:'5px dashed black'}}>
-                <center><h1>Cart</h1>
+            <div className='p-3' style={{position:'fixed',overflowY:'scroll',height:'100%',right:'35px',width:'45%',border:'5px dashed black'}}>
+                <center ><h1>Cart</h1>
                 <h1 style={(cart.length===0)?{display:'block'}:{display:'none'}}>Your Cart is Empty</h1></center>
 
                {
                 cart.length>0 && cart.map((b,ind)=>{
-                    return <div className="card shadow-lg bg-white rounded m-2 bg-info-subtle text-emphasis-info ">
+                    return <div className="card shadow-lg  rounded  m-2 mb-4" style={{backgroundColor:'lightpink'}} >
                        <div className="card-body d-flex justify-content-between" >
                             <center><img src={b.image} style={{height:'90px',width:'90px'}} alt="Card image cap"/></center>
-                            <h5 className="card-title">{b.title}</h5>
-                            <p className="card-text">{b.count*b.price}</p></div>
-                    <div><center style={{position:'absolute',bottom:'20px',left:'250px'}}>
+                            <h5 className="card-title mb-5">{b.title}</h5>
+                            <p className="card-text">${(b.count*b.price).toFixed(2)}</p></div>
+                    <div><center style={{position:'absolute',bottom:'20px',left:'120px'}}>
                     <button className="btn btn-info" onClick={()=>{dec(ind)}} disabled={b.count===1?true:false}>-</button>
                     <span>{b.count}</span>
                     <button className="btn btn-info" onClick={()=>{inc(ind)}}>+</button>
-                    <button className="btn btn-info" style={{position:'absolute',left:'200px'}} onClick={()=>{remove(ind)}}>Remove</button></center>
+                    <button className="btn btn-info" style={{position:'absolute',left:'100px'}} onClick={()=>{remove(ind)}}>Remove</button></center>
                     </div>
-                    
-                    
                     </div>
                     
                 })
                }
-                <div>
+                <div style={(cart.length===0)?{display:"none"}:{display:"block"}}>
                     
-                <div style={{border:'2px dashed black'}}></div>
-                <h3 style={(cart.length===0)?{display:"none"}:{display:"block"}}>Total Price : 
+                <div style={{border:'1px dashed black'}}></div>
+                <b >TOTAL PRICE : <span style={{position:'absolute',left:'85%'}}>$
                 {
                     cart.reduce((t,p)=>{
                         t+=p.price*p.count
                         return t;
-                        setcart([t])
-                    },0)
+                    },0).toFixed(2)
                 }
-                </h3>
+                </span>
+                </b>
                 </div>
             </div>
            
