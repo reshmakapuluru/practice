bc61c14bb7da702c546ca2ea79cf7c873ce1bae8
reshmakapuluru <kapulurureshma024@gmail.com>
2023-09-08 18:00:59 +0530

    image

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import Shoppingcart from './Shoppingcart';
 import Gallery from './Gallery';
 function App() {
   return (
     <div>
       <Gallery></Gallery>
-      {/* <Shoppingcart></Shoppingcart> */}
     </div>
   );
 }
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,35 +1,79 @@
 import React from "react";
-import axios from 'axios';
-function Gallery(){
-    var[photos,setPhotos]=React.useState([])
-    React.useEffect(function(){
-        axios.get('https://jsonplaceholder.typicode.com/photos')
-        .then((res)=>{
-          var photos = res.data
-          console.log(photos)
-          setPhotos([...photos])
-        })
-      },[])
-      function addtogallery(b){
+import axios from "axios";
+import {AiFillCloseCircle } from "react-icons/ai";
 
-      }
+function Gallery(){
+    var [photos,setPhotos]=React.useState([])
+    var [album,setAlbum]=React.useState([])
+    React.useEffect(function(){
+      axios.get('https://picsum.photos/v2/list')
+      .then((res)=>{
+        console.log(res.data)
+        var gallery = res.data.map((y)=>{
+          return {...y,IsImageInGallery:false}
+      });
+        setPhotos(gallery)
+      });
+    },[]);
+    
+    function addtoGallery(i){
+       var temp=[...photos]
+       temp[i].IsImageInGallery=true;
+       setAlbum([...album,temp[i]])
+    } 
+
+    function remove(b,ind){
+        let delConfirm=window.confirm("Are you sure you want to delete this image?")
+        if(delConfirm){
+          var temp=[...album]
+          temp.splice(ind,1)
+          setAlbum(temp)
+        }
+        var x=photos.findIndex((c)=>{
+            return (c.id===b.id)
+        })
+        photos[x].IsImageInGallery=false;
+    }
     return(
         <div className="d-flex flex-wrap">
-          <div style={{width:'55%'}}>
+            <div style={{width:'55%'}}>
+            <center><h1>IMAGES</h1></center>
             <ul className="d-flex flex-wrap">
             {
-              photos.map((a)=>{
-                return <div className="card shadow-lg p-3 mb-5 bg-white rounded m-4 bg-info-subtle text-emphasis-info" style={{width: '14rem'}}>
-                  <img src={a.thumbnailUrl} style={{height:'160px',width:'160px'}} alt="Card image cap"></img>
-                  <div>
-                      <button  className="btn btn-info" onClick={()=>{addtogallery()}}  disabled={a.IsProductInCart?true:false}>Add to cart</button>
+                photos.map((a,i)=>{
+                    return <div className="image  m-3">
+                    
+                    <div className="card-body d-flex flex-column" style={{position:'relative'}}>
+                        <img src={a.download_url} style={{height:'160px',width:'160px',borderRadius:'10px'}}/>
+                        <button className="gallery"  onClick={()=>{addtoGallery(i)}} disabled={a.IsImageInGallery?true:false}>{a.IsImageInGallery?'Added!':'Add to Gallery'}</button>
+                    </div>  
+                    
                   </div>
-                </div>
-              })
+                })
             }
-            </ul>
-          </div>
+           </ul>
+            </div>
+            <div style={{position:'fixed',overflowY:'scroll',height:'100%',right:'35px',width:'45%',border:'5px dashed black'}}>
+                <center><h1>Gallery</h1>
+                <h3 style={(album.length===0)?{display:'block'}:{display:'none'}}>Your Gallery is Empty</h3></center>
+                <ul className="d-flex flex-wrap m-2">
+               {
+                album.length>0 && album.map((b,ind)=>{
+                    return <div className="m-2 mb-4 ">
+                       <div className="card-body d-flex flex-column" style={{position:'relative'}}>
+                        <center><img src={b.download_url} style={{height:'150px',width:'150px'}} alt="Card image cap"/></center>    
+                        <span onClick={()=>{remove(b,ind)}} style={{position:'absolute',right:'-9px',fontSize:'large',top:'-13px',color:'red',backgroundColor:'white',borderRadius:'50%',cursor:'pointer'}}>{<AiFillCloseCircle></AiFillCloseCircle>}</span>
+                        {/* <center><button className="btn btn-info mt-2" onClick={()=>{remove(b,ind)}}>Remove</button></center> */}
+                    </div>
+                  </div>
+                    
+                })
+               }
+               </ul>
+                
+            </div>
+           
         </div>
     )
 }
-export default Gallery
+export default Gallery;
